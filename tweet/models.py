f759a3ff4423ae90601a6abb8482ee17c53161c8
AntLinux <anton-r@live.com>
2021-05-30 22:43:49 +0300

    db changed

--- tweet/models.py
+++ tweet/models.py
@@ -3,7 +3,7 @@ from tweetRoot.utils.root_utils import customer_image_file_path
 
 
 class Tweet(models.Model):
-    content = models.TextField()
+    content = models.TextField(blank=True, null=True)
     image = models.ImageField(default="no_image.jpg", upload_to=customer_image_file_path)
 
     # def __str__(self):
